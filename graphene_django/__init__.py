1c8b9eb09d436cdfe29f5f3bf0319ee2dd44e279
Alexandre Kirszenberg <alexandre.kirszenberg@epita.fr>
2019-08-01 15:03:03 +0200

    Bump version to 2.4.1-acu

--- graphene_django/__init__.py
+++ graphene_django/__init__.py
@@ -1,6 +1,6 @@
 from .types import DjangoObjectType
 from .fields import DjangoConnectionField
 
-__version__ = "2.4.0"
+__version__ = "2.4.1-acu"
 
 __all__ = ["__version__", "DjangoObjectType", "DjangoConnectionField"]
